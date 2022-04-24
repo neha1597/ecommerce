a359ac1cff5264f9884ae11f08652cc77a80b3e8
neha1597 <93374325+neha1597@users.noreply.github.com>
2022-04-24 09:56:57 +0530

    React-App-Ecommerce

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <a href='/'>EShoppers</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
